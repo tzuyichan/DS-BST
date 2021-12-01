3c7f26a4195200e7294e769c65188c467086b898
tzuyichan <F04066028@gs.ncku.edu.tw>
2021-12-02 00:58:35 +0800

    Add bare bone control flow structure to `main()`

--- bst.cpp
+++ bst.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+void do_bst_operations(void);
+void play_finding_meaty(void);
+
 int main(void)
 {
-    std::cout << "Hello world!" << std::endl;
-    std::vector<int> v = {1, 2, 3, 4, 5};
-    for (auto i : v)
-        std::cout << i << " " << std::endl;
+    char mode;
+    while (true)
+    {
+        cout << "(1) Binary search tree\n"
+                "(2) Finding Meaty\n"
+                "(0) Exit"
+             << endl;
+        cin >> mode;
+
+        switch (mode)
+        {
+        case '1':
+            do_bst_operations();
+            break;
+        case '2':
+            play_finding_meaty();
+            break;
+        case '0':
+            return 0;
+        default:
+            cout << "Unknown input." << endl;
+        }
+    }
+
     return 0;
+}
+
+void do_bst_operations(void)
+{
+}
+
+void play_finding_meaty(void)
+{
 }
