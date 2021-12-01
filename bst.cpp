d662e2cf62ba9fecdf329be45f9ef20306a2184d
tzuyichan <F04066028@gs.ncku.edu.tw>
2021-12-01 20:29:50 +0800

    Test Makefile gcc C++11 compilation correctness

--- bst.cpp
+++ bst.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <vector>
 
 int main(void)
 {
     std::cout << "Hello world!" << std::endl;
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    for (auto i : v)
+        std::cout << i << " " << std::endl;
     return 0;
 }
